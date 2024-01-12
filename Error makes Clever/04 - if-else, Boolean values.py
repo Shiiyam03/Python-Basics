276a6fc5bee0ccb58d6375e0f51b5db685136e27
Shiyam R <127540385+Shiiyam03@users.noreply.github.com>
2024-01-12 11:22:41 +0530

    Update and rename 4 if-else, Boolean values.py to 04 - if-else, Boolean values.py

---
+++ Error makes Clever/04 - if-else, Boolean values.py
@@ -0,0 +1,60 @@
+#if-else
+if(True): #True-boolean value
+    print("Yes")
+else:
+    print("no")
+
+if(False): #False-boolean value
+    print("Yes")
+else:
+    print("no")
+
+
+
+
+
+#comparison operator
+print("win"=="win")
+print("Win"=="win")
+print("win"=="winn")
+
+
+
+
+
+""" 1-if rcb=="win" print "Ee sala cup namde"
+else print "valakam pola namaaku cup illa" """
+
+rcb="win"
+if(rcb=="win"):
+    print("Ee sala cup namde")
+else:
+    print("valakam pola namaaku cup illa")
+
+rcb="win"
+if(rcb=="lose"):
+    print("Ee sala cup namde")
+else:
+    print("valakam pola namaaku cup illa")
+
+
+
+
+
+""" 2-Get user input for variable meghna, 
+if meghna==died print "Surya meets Priya"
+else print "Surya weds Meghna" """
+
+meghna=input()
+if(meghna=="died"):
+    print("Surya meets Priya")
+else:
+    print("Surya weds Meghna")
+
+
+
+meghna=input("IS MEGHNA IS ALIVE OR DEAD?")
+if(meghna=="died"):
+    print("Surya meets Priya")
+else:
+    print("Surya weds Meghna")
