4531af421ae25fff1bbee2be39ad9ffe82ec3c75
Shiyam R <127540385+Shiiyam03@users.noreply.github.com>
2024-01-12 10:45:23 +0530

    Update Variables, Datatypes, Operators.py

--- Error makes Clever/Variables, Datatypes, Operators.py
+++ Error makes Clever/Variables, Datatypes, Operators.py
@@ -1,4 +1,5 @@
-#variables
+""" Variables = Used to store data values ( A-Z , a-z ), digits ( 0-9 ),and the underscore character ( _ ).
+First character of a variable name cannot be a digit."""
 a=3
 b=3
 print(a+b)
@@ -7,17 +8,24 @@ A=3
 B=22
 print(A+B)
 
-a="Shiiyam"
+a='Shiiyam' 
 print(a)
 
-#datatype
+
+
+
+""" Datatype = A data type is an attribute associated with a piece of data that tells a computer system 
+how to interpret its value. Eg : int,str,bool. """
 a=3
 print(type(a))
 
 a="Shiiyam"
 print(type(a))
 
-#operators
+
+
+
+""" Operators : +,-,*,/,%,=,== """
 a=3
 b=22
 c=a+b
