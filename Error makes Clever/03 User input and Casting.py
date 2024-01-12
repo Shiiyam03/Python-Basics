a276dfeb94ce5d3a08f20d99310672e9b11e25c9
Shiyam R <127540385+Shiiyam03@users.noreply.github.com>
2024-01-12 11:11:00 +0530

    Update and rename 3 User input and Casting.py to 03 User input and Casting.py

---
+++ Error makes Clever/03 User input and Casting.py
@@ -0,0 +1,98 @@
+""" Casting : 
+          A process that converts a data type into another data type. """
+a="3"
+b="3"
+c=a+b
+print(c)
+
+a=int("3")
+b=int("3")
+c=a+b
+print(c)
+
+
+
+
+
+# Get input (or) User input
+a=input()
+b=input()
+c=a+b
+print(c)
+
+a=int(input())
+b=int(input())
+c=a+b
+print(c)
+
+
+
+
+
+# 1-Get input for variable name and age. Print it.
+name=input()
+age=int(input())
+
+print("My Name is:",name)
+print("My Age is:",age)
+
+
+
+
+
+# 2-Get input for variable name and age,adress. Print it.
+Name=input()
+Age=input()
+Address=input()
+
+print("My Name is:",Name)
+print("My Age is:",Age)
+print("My address is:",Address)
+
+
+
+
+
+""" 3-Get integer input for variable a,b,c.
+Multiply all 3 variables.
+Add all 3 variables.
+Divide the multiplied value by added value.
+Print it. """
+a=int(input())
+b=int(input())
+c=int(input())
+
+x=a*b*c
+y=a+b+c
+z=x/y
+
+print(z)
+
+
+
+
+
+""" 4-Get input for variable name,score,department.
+Get score for 100.
+Divide 100/10 """
+name=input()
+score=int(input())
+department=input()
+x=(score/10)
+
+print("My Name is:",name)
+print("My Score is:",x,"/10")
+print("My department is:",department)
+
+#or
+
+name=input()
+score=int(input())
+department=input()
+
+print("My Name is:",name)
+print("My Score is:",x/10"/10")
+print("My department is:",department)
+
+
+
