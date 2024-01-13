4f9c78d273cd911b8275507b81b40fc6b01d35b3
Shiyam R <127540385+Shiiyam03@users.noreply.github.com>
2024-01-13 23:45:00 +0530

    Update 12 while loop.py

--- Error makes Clever/12 while loop.py
+++ Error makes Clever/12 while loop.py
@@ -1,9 +1,14 @@
+""" The for loop is usually used when the number of iterations is known.
+    The while loop is usually used when the the number of iterations is unknown. """
 i=0
 while(i==0):
     print(i)
     i=i+1
-    
-#1
+
+
+
+
+# 1.Print a number from 1 to 5 using while loop.
 i=int(input("Enter a Number:"))
 while(i<=5):
     print (i)
@@ -14,19 +19,29 @@ while(i<=5):
     print (i)
     i=i+1
 
-#2
+
+
+
+
+# 2.Write a loop statement to print 10 series till 200.  
 i=10
 while(i%10==0) and (i<=200):
     print(i,end=",")
     i=i+10
 
-#3
+
+
+
+# 3.Write a program to print first 10 natural numbers in reverse order.
 i=10
 while(i>0):
     print(i,end=",")
     i=i-1
 
-#4
+
+
+
+# 4.Write a program to find the factorial of a number.
 i=3
 fact=1
 while(i>0):
